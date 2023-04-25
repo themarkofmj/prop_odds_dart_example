c7e00ae44af6c17b03fe116b29b693ddcc9433fa
Matthew Wolfe <m5wolfe@uwaterloo.ca>
2023-04-25 15:51:43 -0400

    initial

---
+++ README.md
@@ -0,0 +1,12 @@
+# Prop_Odds Dart/Flutter Example
+
+An example of how to get the odds for one market of one NBA game today using the [Prop_Odds API](https://www.prop-odds.com/).
+
+The example code is in `bin/myapp.dart`.
+
+### How to Run it
+1. Enter the project.
+2. Install the dependencies with `dart pub get`.
+3. Enter your API key from https://www.prop-odds.com/profile into `bin/myapp.dart` (replace the `MY_API_KEY`).
+4. Run it. `dart run`.
+
---
+++ bin/myapp.dart
@@ -0,0 +1,86 @@
+import 'package:myapp/myapp.dart' as myapp;
+import 'dart:convert' as convert;
+import 'package:http/http.dart' as http;
+import 'package:intl/intl.dart';
+
+String baseUrl = 'api.prop-odds.com';
+String apiKey = '{MY_API_KEY}';
+
+
+Future<Map<String, dynamic>> getRequest(Uri url) async {
+	var response = await http.get(url);
+	if (response.statusCode == 200) {
+		return convert.jsonDecode(response.body) as Map<String, dynamic>;
+	}
+	print('Request failed with status: ${response.statusCode}.');
+	return {};
+}
+
+
+Future<Map<String, dynamic>> getNbaGames() async {
+	var now = new DateTime.now();
+	var formatter = new DateFormat('yyyy-MM-dd');
+	var queryParams = {
+		'date': formatter.format(now),
+		'tz': 'America/New_York',
+		'api_key': apiKey,
+	};
+	var url = Uri.https(baseUrl, '/beta/games/nba', queryParams);
+	return await getRequest(url);
+}
+
+
+Future<Map<String, dynamic>> getGameInfo(String gameId) async {
+	var queryParams = {
+		'api_key': apiKey,
+	};
+	var url = Uri.https(baseUrl, '/beta/game/' + gameId, queryParams);
+	return await getRequest(url);
+}
+
+
+Future<Map<String, dynamic>> getMarkets(String gameId) async {
+	var queryParams = {
+		'api_key': apiKey,
+	};
+	var url = Uri.https(baseUrl, '/beta/markets/' + gameId, queryParams);
+	return await getRequest(url);
+}
+
+
+Future<Map<String, dynamic>> getMostRecentOdds(String gameId, String market) async {
+	var queryParams = {
+		'api_key': apiKey,
+	};
+	var url = Uri.https(baseUrl, '/beta/odds/' + gameId + '/' + market, queryParams);
+	return await getRequest(url);
+}
+
+
+Future<void> main(List<String> arguments) async {
+	var games = await getNbaGames();
+	if (games['games'].length == 0) {
+		print('No games scheduled for today.');
+		return;
+	}
+
+	var firstGame = games['games'][0];
+	var gameId = firstGame['game_id'];
+	// print(firstGame);
+	var gameInfo = await getGameInfo(gameId);
+	// print(gameInfo);
+	
+	var markets = await getMarkets(gameId);
+	// print(markets);
+	if (markets['markets'].length == 0) {
+		print('No markets found.');
+		return;
+	}
+
+	var firstMarket = markets['markets'][0];
+	// print(firstMarket);
+	var odds = await getMostRecentOdds(gameId, firstMarket['name']);
+	print(odds);
+}
+
+
